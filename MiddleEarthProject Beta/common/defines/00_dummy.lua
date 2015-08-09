539793ff8899db874235f585bfd7d5eefb428f77
Moridin997 <bruno_baptista-7@hotmail.com>
2015-08-09 17:59:10 +0100

    ctd fix 2 (it was the commas...)

--- MiddleEarthProject Beta/common/defines/00_dummy.lua
+++ MiddleEarthProject Beta/common/defines/00_dummy.lua
@@ -4,9 +4,9 @@
 
 ----- NDiplomacy -----
 
-NDefines.NDiplomacy.SHORT_REIGN_YEARS_END = 20,
-NDefines.NDiplomacy.LONG_REIGN_YEARS_START = 8000,
-NDefines.NDiplomacy.SHORT_REIGN_OPINION_MULT = 1,				-- Opinion penalty multiplier to short reign years
+NDefines.NDiplomacy.SHORT_REIGN_YEARS_END = 20
+NDefines.NDiplomacy.LONG_REIGN_YEARS_START = 8000
+NDefines.NDiplomacy.SHORT_REIGN_OPINION_MULT = 1				-- Opinion penalty multiplier to short reign years
 
 
 ----- NTitle -----
