3fe663274c3081c1bc2516d69d6d1fccac0b3396
Jakub Šárník <jakub.sarnik@gmail.com>
2015-08-26 21:20:12 +0200

    REMOVED GOVERNMENTS + fixes
    I removed all governments, because they were so buggy and causing CTDs,
    NEEDS REWORK!

--- MiddleEarthProject Beta/common/defines/00_dummy.lua
+++ MiddleEarthProject Beta/common/defines/00_dummy.lua
@@ -43,18 +43,21 @@ NDefines.NTitle.COUNT_AS_VASSAL_PRESTIGE = 0.025 -- previously 0.1
 NDefines.NTitle.DUKE_AS_VASSAL_PRESTIGE = 0.1 -- previously 0.4
 NDefines.NTitle.KING_AS_VASSAL_PRESTIGE = 0.4 -- previously 1.6
 NDefines.NTitle.EMPEROR_AS_VASSAL_PRESTIGE = 3.2     -- Impossible, but needs to be here
+
  -- Monthly prestige from held titles
 NDefines.NTitle.BARON_TITLE_PRESTIGE = 0.001  -- previously 0.025
 NDefines.NTitle.COUNT_TITLE_PRESTIGE = 0.025 -- previously 0.1
 NDefines.NTitle.DUKE_TITLE_PRESTIGE = 0.05 -- previously 0.2
 NDefines.NTitle.KING_TITLE_PRESTIGE = 0.2 -- previously 0.8
 NDefines.NTitle.EMPEROR_TITLE_PRESTIGE = 0.4 -- previously 1.6
+
  -- Monthly prestige LOSS from each unlanded adult son
 NDefines.NTitle.BARON_LANDLESS_SON_PRESTIGE = 0.01 -- previously 0.025
 NDefines.NTitle.COUNT_LANDLESS_SON_PRESTIGE = 0.05 -- previously 0.1
 NDefines.NTitle.DUKE_LANDLESS_SON_PRESTIGE = 0.2 -- previously 0.4
 NDefines.NTitle.KING_LANDLESS_SON_PRESTIGE = 0.4 -- previously 0.8
 NDefines.NTitle.EMPEROR_LANDLESS_SON_PRESTIGE = 0.5 -- previously 1.0
+
  -- Dynasty Prestige is the sum of current and previous holdings of all living and dead members of a dynasty
 NDefines.NTitle.BARON_TITLE_DYNASTY_PRESTIGE = 1 -- previously 1
 NDefines.NTitle.COUNT_TITLE_DYNASTY_PRESTIGE = 3 -- previously 5
